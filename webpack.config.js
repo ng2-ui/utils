cda46f8d0c0911d75bb5c0ec30afd4616fd1d8c4
Allen Kim <allenhwkim@gmail.com>
2017-03-25 14:36:31 -0400

    updated with new module name

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,8 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "[name].umd.js",
-    library: ["[name]"],
+    filename: "utils.umd.js",
+    library: ["utils"],
     libraryTarget: "umd"
   },
   externals: [
