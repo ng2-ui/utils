5433ba538d04fa02777ad84902cb6dc1ab31636c
Allen Kim <allenhwkim@gmail.com>
2017-03-28 15:32:26 -0400

    updated webpack version

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 //noinspection JSAnnotator
 const config = {
   resolve: {
-    extensions: ['', '.ts', '.webpack.js', '.web.js', '.js'],
+    extensions: ['.ts', '.webpack.js', '.web.js', '.js'],
     alias: {
       '@ngui/utils': '../src/index.ts'
     }
@@ -11,15 +11,23 @@ const config = {
   devtool: 'source-map',
   entry: './app/main.ts',
   module: {
-    loaders: [
-      { test: /\.ts$/, loaders: ['ts', 'angular2-template-loader'] },
-      { test: /\.html$/, loader: 'raw' }
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              include: ['src/**/*.ts', 'app/**/*.ts']
+            },
+          },
+          'angular2-template-loader'
+        ],
+      },
+      { test: /\.html$/, use: 'raw' }
     ]
   },
   plugins: [],
-  ts: {
-    include: ['src/**/*.ts', 'app/**/*.ts']
-  },
   output: {
     path: `${__dirname}/build/`,
     publicPath: '/build/',
@@ -31,8 +39,8 @@ if (process.env.NODE_ENV === 'prod') {
   config.plugins = [
     new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
   ];
-  config.module.loaders.push({
-    test: /\.ts$/, loader: 'strip-loader?strip[]=debug,strip[]=console.log'
+  config.module.rules.push({
+    test: /\.ts$/, use: 'strip-loader?strip[]=debug,strip[]=console.log'
   });
 }
 
